e8233389ee45795869012c1b8867240c0257050b
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-18 14:20:04 -0500

    Add routing to 'countries' module by lazy load.
    Create four pages to be used in module navigation.
    Add routing to the four pages in the 'countries-routing.modified.ts' file.
    Configure the 'countries' module to use lazy load, it means the module charge on demand.
      It's not imported in the 'app.module.ts'.
      In the 'app-routing.module.ts' we add the path and define its lazy load.

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CountriesRoutingModule } from './countries-routing.module';
+
+import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { ByCountryPageComponent } from './pages/by-country-page/by-country-page.component';
+import { ByRegionPageComponent } from './pages/by-region-page/by-region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    CountriesRoutingModule
+  ],
+  exports: [
+    ByCapitalPageComponent,
+    ByCountryPageComponent,
+    ByRegionPageComponent,
+    CountryPageComponent
   ]
 })
 export class CountriesModule { }
