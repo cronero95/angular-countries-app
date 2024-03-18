d874a98bad2085f26a16fd8faa9adbbcf8c45640
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-18 17:35:13 -0500

    Create a service module to make a http get request
    Make the request usin the 'HttpClientModule', we return the 'Observable' but don't subscribe to it yet.
    Suscribe to the 'Observable' in the 'by-capital-page.component.ts', where we get a array of type 'Country' as was defined in the interface file.

--- src/app/countries/countries.module.ts
+++ src/app/countries/countries.module.ts
@@ -21,7 +21,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     CountriesRoutingModule,
-    SharedModule
+    SharedModule,
   ],
   exports: [
 
--- src/app/countries/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/countries.interface';
 
 @Component({
   selector: 'countries-by-capital-page',
   templateUrl: './by-capital-page.component.html',
   styles: ``
 })
+
 export class ByCapitalPageComponent {
+
+  public countries: Country[] = [];
+
+  constructor(
+    private countryService: CountriesService
+  ){}
+
   searchByCapital(term: string): void {
-    console.log('From By Capital');
-    console.log({term});
+    this.countryService.searchCapital(term)
+      .subscribe( countries => {
+        this.countries = countries;
+      });
   }
 }
