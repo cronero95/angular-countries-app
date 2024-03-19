7d2e80f10804c13cc5e8b9de7bfabbd70703f5de
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-18 22:15:44 -0500

    Add search by country.

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -20,4 +20,18 @@ export class CountriesService {
       )
   }
 
+  searchCountry(term: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(`${this.apiUrl}/name/${term}`)
+      .pipe(
+        catchError(() => of([]))
+      )
+  }
+
+  searchRegion(term: string): Observable<Country[]> {
+    return this.httpClient.get<Country[]>(`${this.apiUrl}/region/${term}`)
+      .pipe(
+        catchError(() => of([]))
+      )
+  }
+
 }
