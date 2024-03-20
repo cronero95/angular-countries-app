e745efd23c9fbb04de7a3915017b9649bbcaf141
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-19 22:01:31 -0500

    Change browser for buttons.
    Since the possible regions are only 5, instead of using the 'search-box' component, we use 5 buttons.

--- src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Country } from '../../interfaces/countries.interface';
 import { CountriesService } from '../../services/countries.service';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -11,15 +13,19 @@ export class ByRegionPageComponent {
 
   public countries: Country[] = [];
   public isLoading: boolean = false;
+  public regions: Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+  public selectedRegion?: Region;
 
   constructor(
     private countryService: CountriesService
   ){}
 
-  searchByRegion(term: string): void {
+  searchByRegion(region: Region): void {
     this.isLoading = true;
 
-    this.countryService.searchRegion(term)
+    this.selectedRegion = region;
+
+    this.countryService.searchRegion(region)
       .subscribe( countries => {
         this.countries = countries;
         this.isLoading = false;
